82f4710f73dfd65a216af429b9a0783aa11820cd
Akira <akira.eisenbeiss25@imperial.ac.uk>
2025-10-06 15:31:42 +0100

    Fixed

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
+import matplotlib.pyplot as plt
+
 
 __all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
 
@@ -13,5 +15,4 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A,b):
     return A.inv()*b
-
 
